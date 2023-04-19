eb49b504942d054b696a86e6fbdaf61d83dc7e0f
YawarValeriano <yawarvs@gmail.com>
2023-04-19 17:26:59 -0400

    Added functionallity to android

---
+++ MvvmExampleAndroid/ListExampleActivity.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Android.App;
+using Android.OS;
+using Core.ViewModels;
+using MvvmCross.Platforms.Android.Views;
+
+namespace MvvmExampleAndroid
+{
+	[Activity (Label = "ListExampleActivity")]			
+	public class ListExampleActivity : MvxActivity<ListViewModel>
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+            SetContentView(Resource.Layout.activity_list_example);
+            // Create your application here
+        }
+	}
+}
+
--- MvvmExampleAndroid/ViewActivity.cs
+++ MvvmExampleAndroid/ViewActivity.cs
@@ -1,7 +1,9 @@
 ﻿
 
 using Android.App;
+using Android.Content;
 using Android.OS;
+using Android.Widget;
 using Core.ViewModels;
 using MvvmCross.Platforms.Android.Views;
 
@@ -10,11 +12,22 @@ namespace MvvmExampleAndroid
     [Activity(Label = "ViewActivity", MainLauncher = true)]
     public class ViewActivity : MvxActivity<MainViewModel>
     {
+
+        Button button;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+            button = FindViewById<Button>(Resource.Id.NextButton);
 
+            button.Click += OnButtonClick;
+
+        }
+
+        private void OnButtonClick(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(ListExampleActivity));
+            StartActivity(intent);
         }
     }
 }
